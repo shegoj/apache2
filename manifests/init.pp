b1536ee868df58dc55c9cc515cff9b1b9bc2c902
shegoj <shegoj@yahoo.com>
2015-12-02 17:34:41 +0000

    templating apache2

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,14 +3,24 @@
 #
 
 class apache2 (
-  $ensure         = hiera('apache2::ensure', 'installed'),
+  $ensure         =     hiera('apache2::ensure', $apache2::params::version),
+  $configfile     =     hiera ('apache2::configfile', $apache2::params::configfile),
+  $port_number    =     hiera ('apache2::port_number', $apache2::params::port_number)
 ) {
     package { 'httpd':
       ensure => $ensure,
     } ->
     
+    file { '$configfile':
+      ensure    =>    'created',
+      mode      =>    '0755',
+      content   =>    template('httpd.erb'),
+      notify    =>    Service['httpd']
+    
+    }
+    
     service {'httpd':
-      ensure => 'running',
+      ensure    =>    'running',
     }
     
 }
