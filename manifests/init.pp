7f6af90cc357d8a77f97bd51454c7d330e7dbdd6
shegoj <shegoj@yahoo.com>
2015-12-03 09:32:03 +0000

    init class now inherits params

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,13 +6,12 @@ class apache2 (
   $ensure         =     hiera('apache2::ensure', $apache2::params::version),
   $configfile     =     hiera ('apache2::configfile', $apache2::params::configfile),
   $port_number    =     hiera ('apache2::port_number', $apache2::params::port_number)
-) {
+) inherits apache2::params {
     package { 'httpd':
       ensure => $ensure,
     } ->
     
     file { '$configfile':
-      ensure    =>    'created',
       mode      =>    '0755',
       content   =>    template('httpd.erb'),
       notify    =>    Service['httpd']
