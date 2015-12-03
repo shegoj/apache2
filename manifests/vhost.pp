789678200fa881f0c1521453460102b306b901fb
olo10 <shegoj@yahoo.com>
2015-12-03 15:59:17 +0000

    adding vhost class

---
+++ manifests/vhost.pp
@@ -0,0 +1,21 @@
+#class vhost.pp
+#----------------------------
+# to set name or IP based vhosts
+class apache2::vhost (
+  $type         =     hiera('apache2::vhost:type', $apache2::params::vhost_type),
+  $servername   =     hiera('apache2::vhost:servername', $apache2::params::vhost_servername)
+) {
+     if $servername == undef {
+        $actualServername = $hostname
+    }
+    else {
+      $actualServername = $servername
+    }
+  
+    file { 'etc/httpd/conf/httpd2.conf1':
+      content    =>   template  ('apache2/vhost.erb'),
+      mode       =>   '0755',
+      notify     =>   Service['httpd']
+    }
+  
+}
